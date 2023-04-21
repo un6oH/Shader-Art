befda1cc63950ff846ee933c43fa41f537536ec5
un6oy <un6oy.hogun@gmail.com>
2023-04-21 21:01:01 +1000

    gravity ain't worth

--- gravity/fragment-shader.js
+++ gravity/fragment-shader.js
@@ -8,15 +8,24 @@ const DRAW_GRAVITY_FIELD_FS =
 `#version 300 es
 precision highp float;
 
-in vec2 forceVector;
+in vec2 pointToCentre;
 in float distance;
+
+uniform float mass;
 
 out vec4 colour;
 
 void main() {
-  float rCubed = distance * distance * distance;
-  vec2 v = forceVector / rCubed;
-  colour = vec4(v, 1, 1);
+  if (distance < 1.5) {
+    colour = vec4(vec3(0.0), 1.0);
+    return;
+  }
+
+  vec2 direction = normalize(pointToCentre);
+  float distanceSquared = distance * distance;
+  vec2 force = direction * mass / distanceSquared;
+
+  colour = vec4(force, 0.0, 1.0);
 }
 `; 
 
@@ -29,7 +38,7 @@ in vec3 v_colour;
 out vec4 colour;
 
 void main() {
-  
+  colour = vec4(v_colour, 1.0);
 }
 `; 
 
@@ -37,11 +46,14 @@ const DISPLAY_FS =
 `#version 300 es
 precision highp float;
 
-in vec2 position;
+uniform sampler2D image;
+uniform vec2 canvasDimensions;
 
 out vec4 colour;
 
 void main() {
-
+  vec4 value = texture(image, gl_FragCoord.xy / canvasDimensions);
+  // colour = vec4((atan(value) / 3.14159265359 + 0.5).xyz, 1.0);
+  colour = vec4(value.xyz + 0.5, 1.0);
 }
 `;
--- gravity/vertex-shader.js
+++ gravity/vertex-shader.js
@@ -3,11 +3,10 @@ const UPDATE_POSITION_VS =
 
 in vec2 position;
 in vec2 velocity;
-in float mass;
 
-uniform vec2 canvasDimensions;
-uniform vec2 fieldOffset;
-uniform sampler2D aoiTexture;
+uniform vec2 gravityFieldDimensions;
+uniform vec2 canvasOffset;
+uniform sampler2D gravityFieldTexture;
 uniform float gravConstant;
 uniform float deltaTime;
 
@@ -15,9 +14,14 @@ out vec2 newPosition;
 out vec2 newVelocity;
 
 void main() {
-  float distance = length(position);
-  newPosition = position + velocity;
-  newVelocity = velocity + vec2(cos(distance), sin(distance)) / mass;
+  vec2 fieldCoords = position + canvasOffset;
+  vec2 texCoords = fieldCoords / gravityFieldDimensions;
+  vec2 extInfluence = texture(gravityFieldTexture, texCoords).xy;
+
+  vec2 force = gravConstant * extInfluence * 1000.0;
+
+  newVelocity = velocity + force * deltaTime;
+  newPosition = position + newVelocity * deltaTime;
 }
 `;
 
@@ -26,16 +30,25 @@ const DRAW_GRAVITY_FIELD_VS =
 
 in vec2 vertexPosition;
 
-uniform vec2 canvasDimensions;
-uniform int detail;
-uniform float aoiRadius;
+uniform vec2 gravityFieldDimensions;
+uniform vec2 canvasOffset;
+uniform vec2 centre;
+uniform float radius;
 
-out vec2 forceVector;
+out vec2 pointToCentre;
 out float distance;
 
 void main() {
-  forceVector = vertexPosition;
-  distance = 1.0;
+  vec2 fieldCoords = vertexPosition + canvasOffset;
+  vec2 normCoords = fieldCoords / gravityFieldDimensions;
+  vec2 clipSpace = normCoords * 2.0 - 1.0;
+  gl_Position = vec4(clipSpace, 0.0, 1.0);
+
+  int vertexId = gl_VertexID % 72;
+  int point = vertexId % 3;
+
+  pointToCentre = (centre - vertexPosition);
+  distance = (point == 0) ? 0.0 : radius;
 }
 `; 
 
@@ -50,7 +63,11 @@ uniform vec2 canvasDimensions;
 out vec3 v_colour;
 
 void main() {
+  vec2 normCoords = position / canvasDimensions;
+  vec2 clipSpace = normCoords * 2.0 - 1.0;
 
+  gl_Position = vec4(clipSpace * vec2(1.0, -1.0), 0.0, 1.0);
+  v_colour = colour / 255.0;
 }
 `;
 
@@ -59,9 +76,7 @@ const DISPLAY_VS =
 
 in vec2 position;
 
-uniform vec2 canvasDimensions;
-
 void main() {
-  
+  gl_Position = vec4(position, 0.0, 1.0);
 }
 `; 
